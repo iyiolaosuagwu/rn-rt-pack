358ba26a9ac58c47043c666c7e1d7dd465ef56b6
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-03-19 00:38:36 +0100

    fix: testing lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,10 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:prettier/recommended',
     ],
-    plugins: ['react', 'prettier', '@typescript-eslint'],
+    plugins: ['prettier', '@typescript-eslint'],
     parser: '@typescript-eslint/parser',
-    rules: {
-        'react/jsx-uses-react': 'error',
-        'react/jsx-uses-vars': 'error',
-    },
+    // rules: {
+    //     'react/jsx-uses-react': 'error',
+    //     'react/jsx-uses-vars': 'error',
+    // },
 };
