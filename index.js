bfbb88ef40f15a410fccaa233ffced1a23ddac31
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-03-19 13:12:15 +0100

    fix: testted store actions with dispatch and useSelector to extract store data

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, StatusBar} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import store, {persistor} from '@store';
+import {PersistGate} from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => {
+    return (
+        <Provider store={store}>
+            <StatusBar style="dark" />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    );
+};
+
+AppRegistry.registerComponent(appName, () => Root);
